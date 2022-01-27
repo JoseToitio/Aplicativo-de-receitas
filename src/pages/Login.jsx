e55a40d67d9aebbc5391cd79e9b7e706e9b5f87f
Danilo Meneguela <meneguelaprog@hotmail.com>
2022-01-27 18:22:46 -0300

    Css Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,35 +23,70 @@ function Login() {
     localStorage.setItem('cocktailsToken', 1);
   };
   return (
-    <div>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        value={ email }
-        data-testid="email-input"
-        onChange={ ({ target }) => {
-          setEmail(target.value);
-        } }
-      />
-      <input
-        type="password"
-        name="password"
-        id="password"
-        value={ password }
-        data-testid="password-input"
-        onChange={ ({ target }) => {
-          setPassword(target.value);
-        } }
-      />
-      <button
-        type="button"
-        data-testid="login-submit-btn"
-        disabled={ disabled }
-        onClick={ handleClick }
+    <div
+      className="flex bg-green-600 w-screen h-screen
+      items-center text-center"
+    >
+      <form
+        className="bg-green-200 shadow-md rounded px-8 pt-6 pb-8
+        mb-4 mx-4 grid justify-items-center"
       >
-        Enter
-      </button>
+        <span className="text-3xl">LOGIN</span>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3
+              text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              value={ email }
+              data-testid="email-input"
+              type="email"
+              placeholder="Email"
+              onChange={ ({ target }) => {
+                setEmail(target.value);
+              } }
+            />
+          </label>
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full
+              py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none
+              focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+              value={ password }
+              data-testid="password-input"
+              onChange={ ({ target }) => {
+                setPassword(target.value);
+              } }
+            />
+          </label>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4
+            rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            data-testid="login-submit-btn"
+            disabled={ disabled }
+            onClick={ handleClick }
+          >
+            Enter
+          </button>
+        </div>
+      </form>
+
     </div>
   );
 }
