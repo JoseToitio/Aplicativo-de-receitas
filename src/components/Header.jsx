dfb03a67e34dfb84d8a2c57fc8150fba10536f99
Danilo Meneguela <meneguelaprog@hotmail.com>
2022-01-28 17:39:33 -0300

    Criando a barra search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header({ name, showIcon }) {
+  const [showInput, setShowInput] = useState(false);
+
+  function dropInput() {
+    return (
+      <input
+        data-testid="search-input"
+        type="text"
+        placeholder="Pesquise aqui"
+      />
+    );
+  }
+
   function showSearch() {
     return (
-      <button src={ searchIcon } type="button" data-testid="search-top-btn">
+      <button
+        src={ searchIcon }
+        type="button"
+        data-testid="search-top-btn"
+        onClick={ () => {
+          setShowInput(!showInput);
+        } }
+      >
         <img src={ searchIcon } alt="search" />
       </button>
     );
@@ -29,6 +48,7 @@ export default function Header({ name, showIcon }) {
         {name}
       </span>
       { showIcon && showSearch() }
+      { showInput && dropInput() }
     </div>
   );
 }
