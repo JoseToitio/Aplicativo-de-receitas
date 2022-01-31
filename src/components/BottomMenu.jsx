e6311fe4e93239f8b5ee6b776fc8a8480f66ab3a
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-01-31 15:40:56 -0300

    Implentaçao do Menu Inferior

---
+++ src/components/BottomMenu.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function BottomMenu() {
+  // Fonte de pesquisa para usar useHsitory -> https://v5.reactrouter.com/web/api/Hooks
+  const history = useHistory();
+  return (
+    <footer data-testid="footer">
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+      >
+        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="drinks-button" />
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/explore') }
+      >
+        <img data-testid="explore-bottom-btn" src={ exploreIcon } alt="explore-button" />
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/foods') }
+      >
+        <img data-testid="food-bottom-btn" src={ mealIcon } alt="food-button" />
+      </button>
+    </footer>
+  );
+}
+
+export default BottomMenu;
