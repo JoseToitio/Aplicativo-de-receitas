955c1ae0ef214d86cbec40ecf654bb850632f1f8
Toitio <josetoitio@gmail.com>
2022-02-01 19:54:55 -0300

    requisito 17 completo

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -4,14 +4,26 @@ import { useHistory } from 'react-router-dom';
 import GlobalContext from '../context/GlobalContext';
 
 function Items({ page }) {
-  const { valueApiMeals, ValueApiDrinks } = useContext(GlobalContext);
+  const { valueApiMeals,
+    ValueApiDrinks } = useContext(GlobalContext);
   const history = useHistory();
+  const max = 12;
+
+  const filterMaxDrinks = ValueApiDrinks.filter((d, index) => index < max);
+  const filterMaxMeals = valueApiMeals.filter((d, index) => index < max);
+
   const renderFood = () => (
     <div>
-      {valueApiMeals.length === 1
-        ? history.push(`/foods/${valueApiMeals[0].idMeal}`) : valueApiMeals.map((m) => (
-          <div key={ m.idMeals }>
-            <p>{m.strMeal}</p>
+      { valueApiMeals.length === 1
+        ? history.push(`/foods/${valueApiMeals[0].idMeal}`)
+        : filterMaxMeals.map((m, index) => (
+          <div key={ m.idMeal } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ m.strMealThumb }
+              alt={ m.strMeal }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{m.strMeal}</p>
           </div>
         )) }
     </div>
@@ -21,9 +33,14 @@ function Items({ page }) {
     <div>
       {ValueApiDrinks.length === 1
         ? history.push(`/drinks/${ValueApiDrinks[0].idDrink}`)
-        : ValueApiDrinks.map((d) => (
-          <div key={ d.idDrink }>
-            <p>{d.strDrink}</p>
+        : filterMaxDrinks.map((d, index) => (
+          <div key={ d.idDrink } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ d.strDrinkThumb }
+              alt={ d.strDrink }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{d.strDrink}</p>
           </div>
         ))}
     </div>
